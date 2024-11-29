d60835eb6879d72c5066635a015960f05222767c
Aokemi <audrin.yann@gmail.com>
2024-11-29 23:33:53 +0100

    Implemented a searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import Tabs from "./Components/Tabs";
 import Sidebar from "./Components/Sidebar";
 import './index.css';
+import SearchBar from "./Components/Searchbar";
 
 
 function App() {
 
+  const handleSearch = (search) => {
+    console.log("Searching done : ", search) // Searching is done there
+  }
+
   return (
     <div className="app-container">
-
+      
       <Sidebar />
+
 
       <div className="main-content">
 
+        <header className="header">
+          <SearchBar onSearch={handleSearch} />
+        </header>
+        
         <h1> Tabs from categories </h1>
         <Tabs />
 
