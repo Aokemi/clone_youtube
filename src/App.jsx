373fabcb611d6659a9d75f42fd7a7b8ba7317da4
Aokemi <audrin.yann@gmail.com>
2024-12-05 15:06:03 +0100

    Implemented the burger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Tabs from "./Components/Tabs";
 import Sidebar from "./Components/Sidebar";
 import './index.css';
 import SearchBar from "./Components/Searchbar";
+import BurgerMenu from "./Components/BurgerMenu";
 
 
 function App() {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   const handleSearch = (search) => {
     console.log("Searching done : ", search) // Searching is done there
   }
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  }
+
   return (
     <div className="app-container">
-      
-      <Sidebar />
 
+      {isSidebarOpen && <Sidebar />}
 
       <div className="main-content">
 
         <header className="header">
-          <SearchBar onSearch={handleSearch} />
+            <BurgerMenu toggleSidebar={toggleSidebar} />
+            <SearchBar onSearch={handleSearch} />         
         </header>
         
         <h1> Tabs from categories </h1>
